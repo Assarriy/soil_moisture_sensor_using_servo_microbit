a33ee99bd7e69cf316bfd5274e55eba8eadfc0b5
Assariy <113223089+Assarriy@users.noreply.github.com>
2023-03-07 14:03:18 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let Soil = 0
+basic.forever(function () {
+    Soil = Math.idiv(pins.analogReadPin(AnalogPin.P1), 10)
+    serial.writeValue("Humid", Soil)
+    if (Soil <= 98) {
+        basic.showIcon(IconNames.Happy)
+        pins.servoWritePin(AnalogPin.P0, 0)
+    } else {
+        basic.showIcon(IconNames.Sad)
+        pins.servoWritePin(AnalogPin.P0, 180)
+    }
+})
